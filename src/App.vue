e4e5c21ca461f9ba6e0cb29969e8093aaaf068ee
1asagne <74735618+1asagne@users.noreply.github.com>
2022-11-26 00:34:50 +0300

    Fixed layout

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import RootItem from "@/components/RootItem.vue";
 </script>
 
 <template>
-  <n-config-provider :theme="darkTheme">
+  <n-config-provider :theme="darkTheme" abstract>
     <n-message-provider>
-      <n-layout content-style="padding: 1rem; min-height: 100vh; display: flex; flex-direction: column">
+      <n-layout position="absolute" content-style="padding: 1rem; display: flex; flex-direction: column">
         <root-item />
       </n-layout>
     </n-message-provider>
